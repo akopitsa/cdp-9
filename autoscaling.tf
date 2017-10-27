dcb0fb6b9e64b992fc31fbb2db7e276448237613
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-10-27 21:01:37 +0300

    Terraform fmt

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,40 +1,48 @@
 data "template_file" "puppet-server" {
   template = "${file("install_server.sh")}"
+
   vars {
     dns_name = "${aws_elb.my-elb.dns_name}"
   }
 }
+
 resource "aws_launch_configuration" "example-launchconfig" {
-  name_prefix          = "PuppetServer-launchconfig"
-  image_id             = "${lookup(var.AMIS, var.AWS_REGION)}"
-  instance_type        = "t2.micro"
-  key_name             = "${aws_key_pair.mykeypair.key_name}"
-  security_groups      = ["${aws_security_group.myinstance.id}"]
+  name_prefix     = "PuppetServer-launchconfig"
+  image_id        = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type   = "t2.micro"
+  key_name        = "${aws_key_pair.mykeypair.key_name}"
+  security_groups = ["${aws_security_group.myinstance.id}"]
+
   root_block_device {
-    volume_size = "12"
+    volume_size           = "12"
     delete_on_termination = true
   }
+
   #user_data            = "#!/bin/bash\nyum update\nyum -y install epel-release\nyum -y install nginx\nsystemctl start nginx.service\nMYIP=`ifconfig | grep 'inet 10' | awk '{print $2}'`\necho 'this is: '$MYIP > /usr/share/nginx/html/index.html"
   #user_data = "${file("install_server.sh")}"
   user_data = "${data.template_file.puppet-server.rendered}"
-  lifecycle              { create_before_destroy = true }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_autoscaling_group" "example-autoscaling" {
-  name                 = "PuppetServer-autoscaling"
+  name = "PuppetServer-autoscaling"
+
   #vpc_zone_identifier  = ["${aws_subnet.main-public-1.id}", "${aws_subnet.main-public-2.id}"]
-  vpc_zone_identifier  = ["${aws_subnet.main-private-1.id}", "${aws_subnet.main-private-2.id}"]
-  launch_configuration = "${aws_launch_configuration.example-launchconfig.name}"
-  min_size             = 1
-  max_size             = 1
+  vpc_zone_identifier       = ["${aws_subnet.main-private-1.id}", "${aws_subnet.main-private-2.id}"]
+  launch_configuration      = "${aws_launch_configuration.example-launchconfig.name}"
+  min_size                  = 1
+  max_size                  = 1
   health_check_grace_period = 300
-  health_check_type = "ELB"
-  load_balancers = ["${aws_elb.my-elb.name}"]
-  force_delete = true
+  health_check_type         = "ELB"
+  load_balancers            = ["${aws_elb.my-elb.name}"]
+  force_delete              = true
 
   tag {
-      key = "Name"
-      value = "PuppetServer ec2 instance"
-      propagate_at_launch = true
+    key                 = "Name"
+    value               = "PuppetServer ec2 instance"
+    propagate_at_launch = true
   }
 }
--- securitygroup.tf
+++ securitygroup.tf
@@ -1,115 +1,131 @@
 resource "aws_security_group" "myinstance" {
-  vpc_id = "${aws_vpc.main.id}"
-  name = "myinstance"
+  vpc_id      = "${aws_vpc.main.id}"
+  name        = "myinstance"
   description = "security group for my instance"
+
   egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      security_groups = ["${aws_security_group.elb-securitygroup.id}"]
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.elb-securitygroup.id}"]
   }
+
   ingress {
-      from_port = 8140
-      to_port = 8140
-      protocol = "tcp"
-      security_groups = ["${aws_security_group.elb-securitygroup.id}"]
+    from_port       = 8140
+    to_port         = 8140
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.elb-securitygroup.id}"]
   }
+
   tags {
     Name = "SG-for-myinstance"
   }
 }
+
 resource "aws_security_group" "elb-securitygroup" {
-  vpc_id = "${aws_vpc.main.id}"
-  name = "SG-for-elb"
+  vpc_id      = "${aws_vpc.main.id}"
+  name        = "SG-for-elb"
   description = "security group for load balancer"
+
   egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
-      from_port = 8140
-      to_port = 8140
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 8140
+    to_port     = 8140
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags {
     Name = "SG-for-elb"
   }
 }
+
 resource "aws_security_group" "for-nat-instance" {
-  vpc_id = "${aws_vpc.main.id}"
-  name = "security group for-nat-instance"
+  vpc_id      = "${aws_vpc.main.id}"
+  name        = "security group for-nat-instance"
   description = "security group for-nat-instance"
+
   ingress {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      cidr_blocks = ["10.0.4.0/24","10.0.5.0/24","10.0.6.0/24"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   }
+
   ingress {
-      from_port = 443
-      to_port = 443
-      protocol = "tcp"
-      cidr_blocks = ["10.0.4.0/24","10.0.5.0/24","10.0.6.0/24"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   }
+
   ingress {
-      from_port = 8140
-      to_port = 8140
-      protocol = "tcp"
-      cidr_blocks = ["10.0.4.0/24","10.0.5.0/24","10.0.6.0/24"]
+    from_port   = 8140
+    to_port     = 8140
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   }
+
   ingress {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
-      from_port = 8140
-      to_port = 8140
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 8140
+    to_port     = 8140
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
-      from_port = 443
-      to_port = 443
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["10.0.4.0/24","10.0.5.0/24","10.0.6.0/24"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   }
 
   tags {
